741899b13193621c3b8f9f47be50d12e48f9a9a0
«Evgeniia» <girlbysinka@mail.ru>
2022-08-18 20:58:39 +0300

    hw_task25

---
+++ hw_task25/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16
+*/
+Console.Clear();
+
+int userNumber1 = PrintNumber();
+int userNumber2 = PrintNumber();
+
+double degree = Math.Pow(userNumber1, userNumber2);
+Console.WriteLine (degree);
+
+
+int PrintNumber()
+    {
+    int userNumber = new int();
+    Console.WriteLine("Введите пятизначное число:");
+    userNumber = Convert.ToInt32(Console.ReadLine());
+    return userNumber;
+    }
