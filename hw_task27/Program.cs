729d14dd9be584c5df8ada622503d5bc12f8f17e
«Evgeniia» <girlbysinka@mail.ru>
2022-08-18 22:01:37 +0300

    hw_task27

---
+++ hw_task27/Program.cs
@@ -0,0 +1,39 @@
+﻿/*
+Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+Console.Clear();
+
+int userNumber = PrintNumber();
+GetSumDigits (userNumber);
+
+    
+int GetSumDigits (int number)
+    {        
+        int remainder = number % 10;
+        int quotient = number / 10;
+        int sum = remainder;
+        int count = 0;
+        
+        while (quotient > 0)
+            {
+                remainder = quotient % 10;
+                sum = sum + remainder;
+                quotient = quotient / 10;
+                count++;
+            }
+
+        Console.WriteLine($"Сумма цифр в введённом числе равна {sum}");
+        return sum;
+    }
+
+
+int PrintNumber()
+    {
+    int userNumber = new int();
+    Console.WriteLine("Введите пятизначное число:");
+    userNumber = Convert.ToInt32(Console.ReadLine());
+    return userNumber;
+    }
