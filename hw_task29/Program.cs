301fce2ffa9dbc57b8684913e920a5f857152dfa
«Evgeniia» <girlbysinka@mail.ru>
2022-08-21 13:25:48 +0300

    add hw_task29

---
+++ hw_task29/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+(на примерах демонстрация выводов 5 и 3 размерных массивов, вам же нужно сделать 8)
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]
+*/
+Console.Clear();
+
+int[] array = new int[8];
+GetRandomArray (array);
+
+
+void GetRandomArray (int[] array)
+    {
+        for (int i=0; i < array.Length; i++)
+        {
+            array[i] = new Random().Next();
+            Console.Write($"{array[i]} ");
+        }
+    }
